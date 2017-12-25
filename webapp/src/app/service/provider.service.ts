9ddc798480f5d13fb9c2ae7dd654cd3c98fee862
Huyen Vu <quyhuyen.vu@hotmail.com>
2017-12-25 12:06:47 -0500

    update index.html file

--- webapp/src/app/service/provider.service.ts
+++ webapp/src/app/service/provider.service.ts
@@ -2,4 +2,5 @@ import {InjectionToken} from '@angular/core';
 
 export const baseUrlToken = new InjectionToken('baseUrl');
 
-export const baseUrl = "http://localhost:8080/v1";
+// export const baseUrl = "http://localhost:8080/v1";
+export const baseUrl = "https://api-raptorplan.herokuapp.com"
