9bf29c11fcc7bea61995d35b13a133fef820a528
Huyen Vu <quyhuyen.vu@hotmail.com>
2017-12-28 01:02:48 -0500

    change hibernate properties

--- webapp/src/app/service/provider.service.ts
+++ webapp/src/app/service/provider.service.ts
@@ -1,5 +1,5 @@
 import {InjectionToken} from '@angular/core';
-import {environment} from "../../environments/environment.prod";
+import {environment} from "../../environments/environment";
 
 export const baseUrlToken = new InjectionToken('baseUrl');
 
